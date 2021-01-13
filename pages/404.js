f90eb145cd5948c4d977c3e3bc496b5707d8da5a
r-jo <rdrsjck@gmail.com>
2021-01-13 13:23:50 -0800

    Fixed 404

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,12 @@
-import { Box, Link, Heading, Text } from "@chakra-ui/core";
+import Link from "next/link";
 
 export default function Custom404() {
   return (
-      <Box maxW="40rem" p="0rem" m="3rem auto 6rem">
-      <Heading>
+    <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16 mt-4">
+      <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-4 text-black dark:text-white">
         Did you ever hear the tragedy of Darth Plagueis The Wise?
-      </Heading>
-      <Text my={5}>
+      </h3>
+      <p className="text-gray-600 dark:text-gray-400 mb-4 text-lg">
         I thought not. It’s not a story the Jedi would tell you. It’s a Sith
         legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so
         wise he could use the Force to influence the midichlorians to create
@@ -17,10 +17,12 @@ export default function Custom404() {
         eventually, of course, he did. Unfortunately, he taught his apprentice
         everything he knew, then his apprentice killed him in his sleep. Ironic.
         He could save others from death, but not himself.
-      </Text>
-      <Text my={5}>Anyways, you've reached the 404...and nothing else.</Text>
+        <br />
+        <br />
+        Anyways, you've reached the 404...and nothing else.
+      </p>
 
       <Link href="/">← Back to home</Link>
-      </Box>
+    </div>
   );
 }
