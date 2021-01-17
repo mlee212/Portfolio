af56c021671556ed7e92751779667a77e512ce08
r-jo <rdrsjck@gmail.com>
2021-01-17 09:47:58 -0800

    Project card padding

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -3,17 +3,17 @@ import Image from "next/image";
 export default function ProjectCard({ date, category, children, image, href }) {
   return (
     <a href={href} target="_blank">
-      <figure className="md:flex bg-white dark:bg-cardbgdark rounded-lg p-8 md:p-0 shadow-xl mb-4 transition duration-400 ease-in-out transform hover:-translate-y-1">
-        <div className="w-32 h-32 md:w-48 md:h-48 md:p-4 m-auto relative">
+      <figure className="flex bg-white dark:bg-cardbgdark rounded-lg py-4 md:py-0 pr-4 md:px-0 shadow-xl mb-4 transition duration-400 ease-in-out transform hover:-translate-y-1">
+        <div className="w-32 h-32 md:w-48 md:h-48 p-4 m-auto relative">
           <Image
             src={image}
             alt="Product Logo"
             layout="fill"
-            width={200}
-            height={200}
+            width={150}
+            height={150}
           />
         </div>
-        <div className="pt-6 md:py-8 md:pr-8 text-center md:text-left space-y-4">
+        <div className="md:py-6 md:pr-8 space-y-4">
           <figcaption className="font-medium">
             <div className="text-sm text-gray-600 dark:text-gray-400 uppercase">
               {date}
