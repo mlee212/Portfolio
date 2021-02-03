0bf430215afbb7b8f406f4aa9dc36606140e1ccc
r-jo <rdrsjck+gh@gmail.com>
2021-02-03 10:33:08 -0800

    Updated 404

--- pages/404.js
+++ pages/404.js
@@ -5,35 +5,42 @@ import Container from "../components/Container";
 export default function Custom404() {
   return (
     <Container>
-      <div className="flex flex-col justify-center items-center text-center">
-        <div className="max-w-3xl mx-auto flex flex-col justify-center items-center px-6 py-16">
-          <div className="fixed flex z-0 opacity-10">
+      <div className="flex flex-col justify-center items-center">
+        <div className="max-w-3xl mx-auto flex flex-col justify-center items-center px-6">
+          <div className="flex z-0 opacity-10 py-2">
             <Image
               src="/images/deathstar.png"
               alt="Death Star"
-              width={500}
-              height={500}
+              width={200}
+              height={200}
             />
           </div>
-          <h3 className="font-bold text-2xl md:text-4xl mb-16 text-title-light dark:text-title-dark z-10">
+          <h3 className="font-bold text-2xl md:text-4xl pb-4 text-title-light dark:text-title-dark z-10 text-center">
             Did you ever hear the tragedy of Darth Plagueis The Wise?
           </h3>
           <p className="text-text-light dark:text-text-dark mb-4 md:text-lg text-md z-10">
-            I thought not. It’s not a story the Jedi would tell you. It’s a Sith
-            legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and
-            so wise he could use the Force to influence the midichlorians to
-            create life… He had such a knowledge of the dark side that he could
-            even keep the ones he cared about from dying. The dark side of the
-            Force is a pathway to many abilities some consider to be unnatural.
-            He became so powerful… the only thing he was afraid of was losing
-            his power, which eventually, of course, he did. Unfortunately, he
-            taught his apprentice everything he knew, then his apprentice killed
-            him in his sleep. Ironic. He could save others from death, but not
-            himself.
+            I thought not. It’s not a story the Jedi would tell you.
+            <br />
+            <br /> It’s a Sith legend.
             <br />
             <br />
-            Anyways, you've reached the 404, the end of the Jedi territory...I suggest you go back before you
-            succumb to the Dark Side.
+            Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise
+            he could use the Force to influence the midichlorians to create
+            life… He had such a knowledge of the dark side that he could even
+            keep the ones he cared about from dying.
+            <br />
+            <br /> The dark side of the Force is a pathway to many abilities
+            some consider to be unnatural. He became so powerful… the only thing
+            he was afraid of was losing his power, which eventually, of course,
+            he did.
+            <br />
+            <br /> Unfortunately, he taught his apprentice everything he knew,
+            then his apprentice killed him in his sleep. Ironic. He could save
+            others from death, but not himself.
+            <br />
+            <br />
+            Anyways, you've reached the 404, the end of the Jedi territory...I
+            suggest you go back before you succumb to the Dark Side.
           </p>
 
           <Link href="/">
