db7dab8fdbdfb8f91120e4f724086dd1c91fb06c
r-jo <rdrsjck+gh@gmail.com>
2021-02-03 10:12:48 -0800

    Updated color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,59 +4,35 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        transparent: 'transparent',
-        current:'currentColor',
-        title:{
-          light:"#3B3B3A",
-          dark:"#F8F9F6",
-        },
-        primary:{
-          light:"#f4f5f1",
-          dark:"#3e3e3d",
-        },
-        text:{
-          light:"#3e3e3d",
-          dark:"#f4f5f1"
-        },
-        card:{
-          light:"#e0e1dd",
-          dark:"",
-        },
-        link:{
+        transparent: "transparent",
+        current: "currentColor",
+        title: {
           light: "#3B3B3A",
           dark: "#F8F9F6",
         },
-        grey:{
-          light:"rgb(142,142,147)",
-          dark:"rgb(142,142,147)",
+        primary: {
+          light: "#f4f5f1",
+          dark: "#3e3e3d",
         },
-        grey2:{
-          light:"rgb(174,174,178)",
-          dark:"rgb(99,99,102)",
+        text: {
+          light: "#3e3e3d",
+          dark: "#f4f5f1",
         },
-        grey3:{
-          light:"rgb(199,199,204)",
-          dark:"rgb(72,72,74)",
+        card: {
+          light: "#e0e1dd",
+          dark: "#2b2b2a",
         },
-        grey4:{
-          light:"rgb(209,209,214)",
-          dark:"rgb(58,58,60)",
+        subtext: {
+          light: "#636366",
+          dark: "#e8eae2",
         },
-        grey5:{
-          light:"rgb(229,229,234)",
-          dark:"rgb(44,44,46)",
+        link: {
+          light: "#3B3B3A",
+          dark: "#F8F9F6",
         },
-        grey6:{
-          light:"#fbfbfd",
-          dark:"rgb(28,28,30)",
-        },
-        blue:{
-          light:'rgb(0,122,255)',
-          dark:'rgb(0,132,255)'
-        },
-        green:{
-          light:'rgb(52,199,89)',
-          dark:'rgb(48,209,88)'
+        border: {
+          light: "#e0e1dd",
+          dark: "#2b2b2a",
         },
       },
       screens: {
