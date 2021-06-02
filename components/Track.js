5b01f1bee926fcb91012aceee9baf74723fdeac8
r-jo <rdrsjck+gh@gmail.com>
2021-06-02 09:46:36 -0700

    :musical_note: Added album art

--- components/Track.js
+++ components/Track.js
@@ -3,14 +3,16 @@ import styles from '@/styles/track.module.css'
 export default function Track(track) {
   return (
     <div className={styles.track}>
-      <p className={styles.rank}>{track.ranking}</p>
       <div className={styles.info}>
-        <p className={styles.title}>
-          <a href={track.songUrl} target="_blank" rel="noopener noreferrer">
-            {track.title}
-          </a>
-        </p>
-        <p className={styles.artist}>{track.artist}</p>
+        <img src={track.albumArt} className={styles.albumArt} />
+        <div>
+          <p className={styles.title}>
+            <a href={track.songUrl} target="_blank" rel="noopener noreferrer">
+              {track.title}
+            </a>
+          </p>
+          <p className={styles.artist}>{track.artist}</p>
+        </div>
       </div>
     </div>
   )
